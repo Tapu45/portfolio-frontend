6920957feb1e8231f3cb4336490f44d8fa570abd
Tapu45 <rameswarpanda891@gmail.com>
2025-01-20 15:45:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
 import "./App.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { ModeToggle } from "./components/mode-toggle";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProjectView from "./pages/ProjectView";
 import Footer from "./pages/miniComponents/Footer";
 import { ToastContainer } from "react-toastify";
-import { useEffect } from "react";
-// import { initializeAnalytics, logPageView } from "./lib/analytics"; // Import Analytics Utility
 import "react-toastify/dist/ReactToastify.css";
 
-// function App() {
-//   useEffect(() => {
-//     initializeAnalytics(); // Initialize Google Analytics
-//   }, []);
-
+function App() {
   return (
+    
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Router>
-        {/* <PageTracker /> Component to track page views */}
+        {/* <ModeToggle /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/project/:id" element={<ProjectView />} />
@@ -27,17 +23,6 @@ import "react-toastify/dist/ReactToastify.css";
       </Router>
     </ThemeProvider>
   );
-// }
-
-// PageTracker Component for Tracking Route Changes
-// const PageTracker = () => {
-//   const location = useLocation();
-
-//   useEffect(() => {
-//     logPageView(location.pathname); // Log page views on route change
-//   }, [location]);
-
-//   return null;
-// };
+}
 
 export default App;
