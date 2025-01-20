7ebb394cb0c25bec11e9710e92c59b55ed93fe03
Tapu45 <rameswarpanda891@gmail.com>
2025-01-20 16:08:42 +0530

    remove google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import "./App.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "./components/mode-toggle";
-import { BrowserRouter as Router, Routes, Route , useLocation} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProjectView from "./pages/ProjectView";
 import Footer from "./pages/miniComponents/Footer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { initializeAnalytics, logPageView } from "./lib/analytics.js"; 
-import { useEffect } from "react";
+
+
 
 function App() {
-  useEffect(() => {
-    initializeAnalytics(); // Initialize Google Analytics
-  }, []);
+
   return (
     
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-       <PageTracker /> {/* Component to track page views */}
+       
       <Router>
         {/* <ModeToggle /> */}
         <Routes>
@@ -30,14 +28,6 @@ function App() {
     </ThemeProvider>
   );
 }
-const PageTracker = () => {
-  const location = useLocation();
 
-  useEffect(() => {
-    logPageView(location.pathname); // Log page views on route change
-  }, [location]);
-
-  return null;
-};
 
 export default App;
